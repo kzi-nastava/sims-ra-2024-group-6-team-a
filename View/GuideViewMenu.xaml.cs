39f6dad51749c62b20fe5b9f51fd5a6534d9c3c7
StefTrkulja <trkulja.stefander33@gmail.com>
2024-03-07 12:34:27 +0100

    Added TourGuideDTO, TourRepository,GuideViewMenu

---
+++ View/GuideViewMenu.xaml.cs
@@ -0,0 +1,42 @@
+﻿using BookingApp.DTOs;
+using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BookingApp.View
+{
+    /// <summary>
+    /// Interaction logic for GuideViewMenu.xaml
+    /// </summary>
+    public partial class GuideViewMenu : Window
+    {
+        public static ObservableCollection<TourGuideDTO> Tours {  get; set; }
+        public TourGuideDTO SelectedTour { get; set; }
+        public User LoggedUser {  get; set; }
+       
+        private readonly TourRepository _repository;
+        private Location
+
+
+
+
+        public GuideViewMenu(User user; )
+        {
+            InitializeComponent();
+
+        }
+    }
+}
