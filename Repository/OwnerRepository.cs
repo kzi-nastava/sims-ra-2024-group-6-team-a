f8c1037d18cefbb529734dd31575d9cb7a890b2a
PINNGU <dzonic25@gmail.com>
2024-03-23 22:46:55 +0100

    Created a owner model and a repository for saving

---
+++ Repository/OwnerRepository.cs
@@ -0,0 +1,57 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+
+namespace BookingApp.Repository
+{
+    public class OwnerRepository
+    {
+        private const string FilePath = "../../../Resources/Data/owners.csv";
+
+        private readonly Serializer<Owner> _serializer;
+
+        private List<Owner> _owners;
+
+        public OwnerRepository()
+        {
+            _serializer = new Serializer<Owner>();
+            _owners = _serializer.FromCSV(FilePath);
+        }
+
+        public List<Owner> GetAll()
+        {
+            return _serializer.FromCSV(FilePath);
+        }
+
+        public Owner Save(Owner Owner)
+        {
+            Owner.Id = NextId();
+            _owners = _serializer.FromCSV(FilePath);
+            _owners.Add(Owner);
+            _serializer.ToCSV(FilePath, _owners);
+            return Owner;
+        }
+
+        public int NextId()
+        {
+            _owners = _serializer.FromCSV(FilePath);
+            if (_owners.Count < 1)
+            {
+                return 1;
+            }
+            return _owners.Max(c => c.Id) + 1;
+        }
+
+        public void Delete(Owner Owner)
+        {
+            _owners = _serializer.FromCSV(FilePath);
+            Owner founded = _owners.Find(c => c.Id == Owner.Id);
+            _owners.Remove(founded);
+            _serializer.ToCSV(FilePath, _owners);
+        }
+
+    }
+}
