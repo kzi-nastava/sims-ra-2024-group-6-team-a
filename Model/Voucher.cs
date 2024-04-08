a467ada631c78035389e57c10b14d7ea4d739812
StefTrkulja <trkulja.stefander33@gmail.com>
2024-04-08 11:04:52 +0200

    Added Tour Cancelation feature

---
+++ Model/Voucher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Resources;
+using BookingApp.Serializer;
+
+
+
+namespace BookingApp.Model
+{
+    public class Voucher : ISerializable
+    {
+        public int Id { get; set; }
+ 
+        public String TouristName { get; set; }
+        public String TouristSurname { get; set; }
+        public int TouristBirth {  get; set; }
+
+        public DateTime IssuingDate { get; set; }
+
+        public Voucher() { }
+
+
+      
+        public Voucher(int id, string touristName, string touristSurname, int touristBirth, DateTime issuingDate)
+        {
+            Id = id;
+            TouristName = touristName;
+            TouristSurname = touristSurname;
+            TouristBirth = touristBirth;
+            IssuingDate = issuingDate;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), TouristName, TouristSurname, TouristBirth.ToString(),  IssuingDate.ToString()};
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+
+            Id = Convert.ToInt32(values[0]);
+            TouristName = values[1];
+            TouristSurname = values[2];
+            TouristBirth = int.Parse(values[3]);
+            IssuingDate = DateTime.Parse(values[4]);
+
+
+        }
+
+    
+    }
+
+}
---
+++ View/GuideView/Components/AllToursCard.xaml.cs
@@ -0,0 +1,90 @@
+﻿using BookingApp.Model;
+using BookingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookingApp.View.GuideView.Components
+{
+    /// <summary>
+    /// Interaction logic for AllToursCard.xaml
+    /// </summary>
+    public partial class AllToursCard : UserControl
+    {
+
+        TourScheduleRepository _tourScheduleRepository;
+        TourGuestRepository _tourGuestRepository;
+        VoucherRepository _voucherRepository;
+        TourRepository _tourRepository;
+
+        TourReservationRepository _tourReservationRepository;
+
+        public event EventHandler TourCanceledCard;
+        public AllToursCard()
+        {
+            InitializeComponent();
+            _tourScheduleRepository = new TourScheduleRepository();
+            _tourGuestRepository = new TourGuestRepository();
+            _voucherRepository = new VoucherRepository();
+            _tourRepository = new TourRepository();
+            _tourReservationRepository  = new TourReservationRepository();
+        }
+
+        private void DeleteTourMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TourSchedule tourSchedule = _tourScheduleRepository.GetById(Convert.ToInt32(textBoxId.Text));
+            List<TourGuests> guests = _tourGuestRepository.GetAllByTourId(tourSchedule.Id);
+
+
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to cancel your tour?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (Convert.ToDateTime(txtTourStart.Text) > DateTime.Now.AddHours(48))
+            {
+                if (result == MessageBoxResult.Yes)
+                {
+                    // Code to cancel the tour
+                    if (guests.Count != 0)
+                    {
+                        _voucherRepository.SaveAllGuests(guests);
+                        _tourReservationRepository.Delete(_tourReservationRepository.GetById(tourSchedule.TourId));
+                    }
+
+                    foreach (TourGuests guest in guests)
+                    {
+                        _tourGuestRepository.Delete(guest);
+                    }
+
+
+                    if (_tourScheduleRepository.GetAllByTourId(tourSchedule.TourId).Count == 1)
+                    {
+                        _tourRepository.Delete(_tourRepository.GetById(tourSchedule.TourId));
+                    }
+
+                    _tourScheduleRepository.Delete(tourSchedule);
+                    HandleTourCanceledEvent();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selected tour can't be canceled(less than 48 hours til tour comences");
+            }
+
+        }
+
+        public void HandleTourCanceledEvent()
+        {
+            TourCanceledCard?.Invoke(this,EventArgs.Empty);
+        }
+
+    }
+}
