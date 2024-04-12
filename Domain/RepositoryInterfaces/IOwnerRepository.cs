9397f80b3cc4219afe06c3fc2f051ff6e69746f8
PINNGU <dzonic25@gmail.com>
2024-04-12 14:34:54 +0200

    Finished owner interfaces for repos

---
+++ Domain/RepositoryInterfaces/IOwnerRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookingApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.RepositoryInterfaces
+{
+    public interface IOwnerRepository
+    {
+        public List<Owner> GetAll();
+        public Owner Save(Owner Owner);
+        public int NextId();
+        public void Delete(Owner Owner);
+        public Owner Update(Owner owner);
+    }
+}
