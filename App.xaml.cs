8d0217bd6760e34ea014f97587603e89d0e14983
StefTrkulja <trkulja.stefander33@gmail.com>
2024-04-15 10:41:46 +0200

    Added DP INJ functionality

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BookingApp.ApplicationServices;
+using BookingApp.Domain.RepositoryInterfaces;
+using BookingApp.Repository;
+using BookingApp.RepositoryInterfaces;
+using BookingApp.View;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +19,60 @@ namespace BookingApp
     /// </summary>
     public partial class App : Application
     {
+        private static IServiceProvider _serviceProvider;
+
+        public static IServiceProvider ServiceProvider => _serviceProvider;
+    
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            IServiceCollection services = new ServiceCollection();
+            ConfigureWindows(services);
+            ConfigureServices(services);
+            ConfigureRepositories(services);
+
+            _serviceProvider = services.BuildServiceProvider();
+
+            var signInForm = _serviceProvider.GetRequiredService<SignInForm>();
+            signInForm.Show();
+
+        }
+        private void ConfigureWindows(IServiceCollection services)
+        {
+            services.AddTransient<SignInForm>();
+        }
+
+        private void ConfigureRepositories(IServiceCollection services)
+        {
+
+            services.AddSingleton<ITourRepository, TourRepository>();
+            services.AddSingleton<ITourReservationRepository, TourReservationRepository>();
+            services.AddSingleton<ITourGuestRepository, TourGuestRepository>();
+            services.AddSingleton<IReservationChangeRepository, ReservationChangeRepository>();
+            services.AddSingleton<ITourReviewRepository, TourReviewRepository>();   
+            services.AddSingleton<ITourScheduleRepository, TourScheduleRepository>();
+            services.AddSingleton<IVoucherRepository, VoucherRepository>(); 
+            services.AddSingleton<ICheckpointRepository, CheckpointRepository>();   
+            services.AddSingleton<IImageRepository, ImageRepository>();
+            services.AddSingleton<ILocationRepository,LocationRepository>();    
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<TourService>();
+            services.AddSingleton<CheckpointService>();
+            services.AddSingleton<TourGuestService>();
+            services.AddSingleton<TourReservationService>();
+            services.AddSingleton<TourReviewService>(); 
+            services.AddSingleton<TourScheduleService>();
+            services.AddSingleton<TourService>();
+            services.AddSingleton<VoucherService>();
+            services.AddSingleton<ImageService>();
+            services.AddSingleton<LocationService>();   
+
+        }
+
+
     }
+
 }
---
+++ ApplicationServices/LocationService.cs
@@ -0,0 +1,59 @@
+﻿using BookingApp.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Repository;
+using BookingApp.Model;
+using BookingApp.Observer;
+using BookingApp.Serializer;
+
+
+namespace BookingApp.ApplicationServices
+{
+    public class LocationService
+    {
+        private ILocationRepository _locationRepository;
+
+
+        public LocationService()
+        {
+            _locationRepository = new LocationRepository();
+        }
+
+        public List<Location> GetAll()
+        {
+            return _locationRepository.GetAll();
+        }
+
+        public Location Save(Location location)
+        {
+            return _locationRepository.Save(location);
+        }
+
+        public int NextId()
+        {
+           return _locationRepository.NextId();
+        }
+
+        public void Delete(Location location)
+        {
+            _locationRepository.Delete(location);
+        }
+
+
+        public Location GetById(int id)
+        {
+
+            return _locationRepository.GetById(id);  
+        }
+
+        public Location GetByAccommodation(Accommodation accommodation)
+        {
+            return _locationRepository.GetByAccommodation(accommodation);
+        }
+
+    }
+
+}
