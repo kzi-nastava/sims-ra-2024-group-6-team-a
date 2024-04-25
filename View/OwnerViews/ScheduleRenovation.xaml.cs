762b5b19a750e1ec538abd3c551a38ac3966217f
PINNGU <dzonic25@gmail.com>
2024-04-25 14:44:58 +0200

    Changed to viewmodel for scheduling

--- View/OwnerViews/ScheduleRenovation.xaml.cs
+++ View/OwnerViews/ScheduleRenovation.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using BookingApp.ApplicationServices;
 using BookingApp.DTOs;
 using BookingApp.Model;
+using BookingApp.ViewModels.OwnerViewModel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -22,67 +23,32 @@ namespace BookingApp.View.OwnerViews
     /// <summary>
     /// Interaction logic for ScheduleRenovation.xaml
     /// </summary>
-    public partial class ScheduleRenovation : Window,INotifyPropertyChanged
+    public partial class ScheduleRenovation : Window
     {
-
-        List<DateOnly?> availableDates = new List<DateOnly?> ();
-
-        public event PropertyChangedEventHandler? PropertyChanged;
-        protected virtual void OnPropertyChanged(string name)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(name));
-            }
-        }
-        public AccommodationOwnerDTO Accommodation {  get; set; }
-        public List<ReservationOwnerDTO> Reservations { get; set; }
-        private bool isPeriodFound;
+        ScheduleRenovationVM ViewModel { get; set; }
 
         public ScheduleRenovation(AccommodationOwnerDTO Accommodation,List<ReservationOwnerDTO> Reservations)
         {
-            this.Accommodation = Accommodation;
-            this.Reservations = Reservations;
-            isPeriodFound = false;
-            DataContext = this;
+            ViewModel = new ScheduleRenovationVM(Accommodation, Reservations);
+            DataContext = ViewModel;
             InitializeComponent();
         }
 
         private void Find_Click(object sender, RoutedEventArgs e)
         {
-            int duration;
-            if (StartDatePicker.SelectedDate != null && EndDatePicker.SelectedDate != null && int.TryParse(DurationPicker.Text, out duration))
-            {
-                availableDates = RenovationService.GetInstance().GetAvailableDatesForRenovation(Reservations, DateOnly.FromDateTime((DateTime)StartDatePicker.SelectedDate), DateOnly.FromDateTime((DateTime)EndDatePicker.SelectedDate), duration);
-                if (availableDates[0] != null)
-                {
-                    HelpBox.Text = "There is an available period between " + ((DateOnly)availableDates[0]).ToString("dd MMMM,yyyy") + " and " + ((DateOnly)availableDates[1]).ToString("dd MMMM,yyyy");
-                    isPeriodFound = true;
-                }
-                else
-                {
-                    HelpBox.Text = "There is no available period between selected dates for the duration.";
-                    isPeriodFound = false;
-                }
-            }
-            else
-            {
-                HelpBox.Text = "Please make sure you have selected dates and entered a number for the duration.";
-                isPeriodFound = false;
-            }
+            HelpBox.Text = ViewModel.FindDates(StartDatePicker.SelectedDate,EndDatePicker.SelectedDate,DurationPicker.Text);
 
         }
 
         private void Confirm_Click(object sender, RoutedEventArgs e)
         {
             int duration;
-            if(isPeriodFound && StartDatePicker.SelectedDate != null && EndDatePicker.SelectedDate != null && int.TryParse(DurationPicker.Text, out duration))
+            if(ViewModel.isPeriodFound && StartDatePicker.SelectedDate != null && EndDatePicker.SelectedDate != null && int.TryParse(DurationPicker.Text, out duration))
             {
                 if(MessageBox.Show("Confirm renovation?", "Schedule", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
                 {
-                    
-                    AccommodationRenovation renovation = new AccommodationRenovation(RenovationService.GetInstance().NextId(),TitleText.Text,Accommodation.Id, (DateOnly)availableDates[0], (DateOnly)availableDates[1]);
-                    RenovationService.GetInstance().Save(renovation);
+
+                    ViewModel.ConfirmSchedule(TitleText.Text);
                     Close();
                 }
             }
--- ViewModels/OwnerViewModel/ScheduleRenovationVM.cs
+++ ViewModels/OwnerViewModel/ScheduleRenovationVM.cs
@@ -1,12 +1,70 @@
-﻿using System;
+﻿using BookingApp.ApplicationServices;
+using BookingApp.DTOs;
+using BookingApp.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BookingApp.ViewModels.OwnerViewModel
 {
-    internal class ScheduleRenovationVM
+    public class ScheduleRenovationVM : INotifyPropertyChanged
     {
+        List<DateOnly?> availableDates = new List<DateOnly?>();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        public AccommodationOwnerDTO Accommodation { get; set; }
+        public List<ReservationOwnerDTO> Reservations { get; set; }
+        public bool isPeriodFound;
+
+        public ScheduleRenovationVM(AccommodationOwnerDTO Accommodation, List<ReservationOwnerDTO> Reservations)
+        {
+            this.Accommodation = Accommodation;
+            this.Reservations = Reservations;
+            isPeriodFound = false;
+        }
+
+        public string FindDates(DateTime? StartDate,DateTime? EndDate,String Duration)
+        {
+            int duration;
+            if (StartDate != null && EndDate != null && int.TryParse(Duration, out duration))
+            {
+                availableDates = RenovationService.GetInstance().GetAvailableDatesForRenovation(Reservations, DateOnly.FromDateTime((DateTime)StartDate), DateOnly.FromDateTime((DateTime)EndDate), duration);
+                if (availableDates[0] != null)
+                {
+                    isPeriodFound = true;
+                    return "There is an available period between " + ((DateOnly)availableDates[0]).ToString("dd MMMM,yyyy") + " and " + ((DateOnly)availableDates[1]).ToString("dd MMMM,yyyy");
+                    
+                }
+                else
+                {
+                    isPeriodFound = true;
+                    return  "There is no available period between selected dates for the duration.";
+                    
+                }
+            }
+            else
+            {
+                isPeriodFound = true;
+                return  "Please make sure you have selected dates and entered a number for the duration.";
+                
+            }
+        }
+
+        internal void ConfirmSchedule(String Title)
+        {
+            AccommodationRenovation renovation = new AccommodationRenovation(RenovationService.GetInstance().NextId(), Title, Accommodation.Id, (DateOnly)availableDates[0], (DateOnly)availableDates[1]);
+            RenovationService.GetInstance().Save(renovation);
+        }
     }
 }
