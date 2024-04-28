38861dccdc55ad4508493e12a3f92959c3042514
PINNGU <dzonic25@gmail.com>
2024-04-28 18:47:36 +0200

    Additional renovation comment with guest reviews

--- ViewModels/OwnerViewModel/GuestsReviewVM.cs
+++ ViewModels/OwnerViewModel/GuestsReviewVM.cs
@@ -13,11 +13,20 @@ namespace BookingApp.ViewModels
         public int Cleanliness { get; set; }
         public int Correctness { get; set; }
         public string AdditionalComment { get; set; }
+        public string RenovationRec {  get; set; }
         public GuestsReviewVM(OwnerReview ownerReview) 
         {
             this.Cleanliness = ownerReview.Cleanliness;
             this.Correctness = ownerReview.Correctness;
             this.AdditionalComment = ownerReview.AditionalComment;
+
+            if (ownerReview.Urgency == "Level5" && ownerReview.Urgency == "Level4")
+                this.RenovationRec = "The guest thinks that this accommodation must have a renovation urgently.";
+            else if (ownerReview.Urgency == "Level3" && ownerReview.Urgency == "Level2")
+                this.RenovationRec = "The guest thinks that this accommodation could use a renovation soon.";
+            else
+                this.RenovationRec = "The guest thinks that this accommodation does not require a renovation.";
+
         }
 
     }
